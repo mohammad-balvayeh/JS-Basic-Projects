f7921fd10a5999a046142b37c9e94a2f43dc2d51
mohammad.balvayeh <mohammad.balvayeh@gmail.com>
2021-08-05 21:32:20 +0430

    Added JS Code And Complate Simple Password Generater

---
+++ 16-Simple-Password-Generater/index.js
@@ -0,0 +1,89 @@
+const characters = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const passwordLength = 12;
+
+const showPassword = document.querySelector(".show-password");
+const generateBtn = document.querySelector(".generate-btn");
+
+generateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let password = "";
+  for (let index = 0; index < 12; index++) {
+    const randNum = Math.floor(Math.random() * characters.length);
+    password += characters[randNum];
+  }
+  showPassword.textContent = password;
+});
